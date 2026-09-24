13276751d0879e327bab645212fe3c4460216ef6
David Grant <support@wpwham.com>
2026-09-24 02:48:55 -0700

    v1.4.10

--- product-visibility-by-country-for-woocommerce.php
+++ product-visibility-by-country-for-woocommerce.php
@@ -3,14 +3,12 @@
 Plugin Name: Product Visibility by Country for WooCommerce
 Plugin URI: https://wpwham.com/products/product-visibility-by-country-for-woocommerce/
 Description: Display WooCommerce products by customer's country.
-Version: 1.4.9
+Version: 1.4.10
 Author: WP Wham
 Author URI: https://wpwham.com/
 Text Domain: product-visibility-by-country-for-woocommerce
 Domain Path: /langs
-WC requires at least: 3.0
-WC tested up to: 7.8
-Copyright: © 2018-2023 WP Wham. All rights reserved.
+Copyright: © 2018-2026 WP Wham. All rights reserved.
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 */
@@ -20,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'WPWHAM_PRODUCT_VISIBILITY_BY_COUNTRY_VERSION', '1.4.9' );
+define( 'WPWHAM_PRODUCT_VISIBILITY_BY_COUNTRY_VERSION', '1.4.10' );
 define( 'WPWHAM_PRODUCT_VISIBILITY_BY_COUNTRY_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) . '/' );
 
 if ( ! class_exists( 'Alg_WC_PVBC' ) ) :
@@ -29,7 +27,7 @@ if ( ! class_exists( 'Alg_WC_PVBC' ) ) :
  * Main Alg_WC_PVBC Class
  *
  * @class   Alg_WC_PVBC
- * @version 1.4.9
+ * @version 1.4.10
  * @since   1.0.0
  */
 final class Alg_WC_PVBC {
@@ -42,7 +40,7 @@ final class Alg_WC_PVBC {
 	 * @var   string
 	 * @since 1.0.0
 	 */
-	public $version = '1.4.9';
+	public $version = '1.4.10';
 
 	/**
 	 * @var   Alg_WC_PVBC The single instance of the class
@@ -70,7 +68,7 @@ final class Alg_WC_PVBC {
 	/**
 	 * Alg_WC_PVBC Constructor.
 	 *
-	 * @version 1.3.0
+	 * @version 1.4.10
 	 * @since   1.0.0
 	 * @access  public
 	 */
@@ -116,7 +114,7 @@ final class Alg_WC_PVBC {
 	/**
 	 * Include required core files used in admin and on the frontend.
 	 *
-	 * @version 1.1.0
+	 * @version 1.4.10
 	 * @since   1.0.0
 	 */
 	public function includes() {
--- readme.txt
+++ readme.txt
@@ -2,8 +2,8 @@
 Contributors: wpwham
 Tags: woocommerce, product, visibility, country, woo commerce
 Requires at least: 4.4
-Tested up to: 6.2
-Stable tag: 1.4.9
+Tested up to: 7.1
+Stable tag: 1.4.10
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -41,6 +41,11 @@ There are options in plugin to hide products by:
 3. Start by visiting plugin settings at "WooCommerce > Settings > Product Visibility by Country".
 
 == Changelog ==
+
+= 1.4.10 - 2026-09-24 =
+* UPDATE: remove United Kingdom from EU countries setting.
+* FIX: sanitize HTML in shortcodes.
+* FIX: PHP notice.
 
 = 1.4.9 - 2023-07-15 =
 * FIX: PHP 8.2 notices.
